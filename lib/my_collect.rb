1a8c2a79ca0871d7b0a20a00277807d593523af5
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-13 19:19:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,7 @@
 def
 collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang| lang.upcase
+my_collect(collection).each do |lang| lang.upcase
+collection.split(collection)
+yield
+lang+= 1
 end
